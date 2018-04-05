8128153434433e18dcda978a462f6c4da7cdc2ac
Love F <lovef.code@gmail.com>
2018-04-05 19:11:07 +0200

    Boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
+import { MatToolbarModule } from '@angular/material'
 
 import { ServiceWorkerModule } from '@angular/service-worker'
 import { AppComponent } from './app.component'
@@ -15,6 +16,7 @@ import { SnakeComponent } from './snake/snake.component'
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
