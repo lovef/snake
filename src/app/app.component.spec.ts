cd2950ebdf94932001fd6d98bb92865dd06683a1
Love F <lovef.code@gmail.com>
2018-04-05 19:11:07 +0200

    Boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed, async } from '@angular/core/testing'
 import { AppComponent } from './app.component'
+import { SnakeComponent } from './snake/snake.component'
+import { MatToolbarModule } from '@angular/material'
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatToolbarModule,
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SnakeComponent
       ],
     }).compileComponents()
   }))
@@ -16,12 +22,12 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('app')
+    expect(app.title).toEqual('Snake')
   }))
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a mat-toolbar tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!')
+    expect(compiled.querySelector('mat-toolbar').textContent).toContain('Snake')
   }))
 })
